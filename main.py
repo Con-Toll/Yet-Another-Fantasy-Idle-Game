a2da6af00b410275a6e9a767b8a354dbcf8fdc23
danceinthestars <haruzz1337@gmail.com>
2024-04-21 16:54:38 +0800

    Initialise repository.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
+from pygame.locals import *
 import sys
+
 pygame.init()
 
 
 screen_width = 1000
 screen_height = 800
-screen = pygame.display.set_mode((screenwidth, screenheight))
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Yet Another Idle Clicker")
 
