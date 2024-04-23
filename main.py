cf336db2a042d8b16b700a1f39c4b546043c0505
danceinthestars <haruzz1337@gmail.com>
2024-04-23 16:23:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,36 @@ import sys
 pygame.init()
 
 
+# -- VARIABLES --
+# Screen info
 screen_width = 1000
 screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Yet Another Idle Clicker")
 
+# FPS
+fps = 60
+pygame.time.Clock(fps)
+
+# Colours
 white = (255, 255, 255)
 black = (0, 0, 0)
 
 font = pygame.font.SysFont(None, 36)
 main_menu = 1
 
+# Clicking
 clicks = 0
 click_power = 1
 auto_click_power = 0
-auto_click_interval = 1000  # in milliseconds
+click_check_interval = 1000  # in milliseconds
 last_auto_click_time = pygame.time.get_ticks()
 
+# Currency
+gold = 0
+
+# Game loop \o/
 running = True
 while running:
     screen.fill(white)
@@ -41,7 +53,7 @@ while running:
 
     # Auto-click
     current_time = pygame.time.get_ticks()
-    if current_time - last_auto_click_time >= auto_click_interval:
+    if current_time - last_auto_click_time >= click_check_interval:
         clicks += auto_click_power
         last_auto_click_time = current_time
 
