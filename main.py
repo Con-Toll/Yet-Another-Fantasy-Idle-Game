36822c624cacdd7ea35dbbdbd5e8aca9d7e7dc10
Con-Toll <1221109031@student.mmu.edu.my>
2024-04-23 17:02:31 +0800

    Update main.py
    Just button

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from pygame.locals import *
+import pygame_gui
 import sys
+import os
 
 pygame.init()
 
@@ -22,16 +23,56 @@ auto_click_power = 0
 auto_click_interval = 1000  # in milliseconds
 last_auto_click_time = pygame.time.get_ticks()
 
+def champ_menu(x,y,width,height):
+     champ_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((x,y), (width, height)),
+                                               text='Champion',
+                                               manager=window,
+                                               )
+    return champ_button
+
+
+#button
+button_layout_rect = pygame.Rect(30,20,100,20)
+window = pygame_gui.UIManager((screen_width, screen_height),theme_path='theme.json')
+
+
+
+champ_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((100, 50), (200, 50)),
+                                             text='Champion',
+                                             manager=window,
+                                             )
+upgrade_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((400, 700), (200, 50)),
+                                             text='Upgrade',
+                                             manager=window,
+                                             )
+misc_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((700, 700), (200, 50)),
+                                             text='Misc.',
+                                             manager=window,
+                                             )
+
+
+
+clock = pygame.time.Clock()
 running = True
 while running:
     screen.fill(white)
+    time_delta = clock.tick(60)/1000.0
 
     # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            window.process_events(event)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:  # Left mouse button
+                mouse_pos = pygame.mouse.get_pos()
+            if champ_button.rect.collidepoint(mouse_pos):
+                champ_menu(100,200,200,700)
+            elif upgrade_button.rect.collidepoint(mouse_pos):
+                print("Upgrade button pressed")
+            elif misc_button.rect.collidepoint(mouse_pos):
+                print("Misc. button pressed")
+            else:
                 clicks += click_power
 
     # Draw text
@@ -43,7 +84,11 @@ while running:
     if current_time - last_auto_click_time >= auto_click_interval:
         clicks += auto_click_power
         last_auto_click_time = current_time
+    
 
+    window.update(time_delta)
+    window.draw_ui(screen)
+    
     pygame.display.flip()
 
 pygame.quit()
