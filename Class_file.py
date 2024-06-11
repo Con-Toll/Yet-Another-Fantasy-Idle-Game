4f6c948d261ee3a7160819fe1fd1aac659d09827
Con-Toll <1221109031@student.mmu.edu.my>
2024-06-11 19:40:13 +0800

    Lucky wheel
    - Just create a class for the lucky wheel, got some help to draw the triangle and put it side by side

--- Class_file.py
+++ Class_file.py
@@ -276,3 +276,55 @@ class QTE(pygame.sprite.Sprite):
     
     
     
+class Triangle():
+    def __init__(self,center_x,center_y,radius,angle,length,height,color):
+        self.center_x = center_x
+        self.center_y = center_y
+        self.radius = radius
+        self.angle = angle
+        self.angular_velocity = 0
+        self.length = length
+        self.height = height
+        self.color = color
+        self.rotated_points = []
+        self.fallen_word = ["ADD 100000","ADD CHAMPION","ADD BOY","ADD 5223","ADD 566O2","ADD 23211","ADD 2321","ADD 23133","NONE","NONE"] #Add the bonus thing that you want
+        
+    def update(self):
+        self.angle +=self.angular_velocity
+        
+    def draw(self,screen):
+        x = self.center_x + self.radius * math.cos(math.radians(self.angle))
+        y   = self.center_x + self.radius * math.sin(math.radians(self.angle))
+        angle_center = math.atan2(self.center_y - y,self.center_x - x)
+        angle_center = math.degrees(angle_center)
+        
+        triangle_width = 2* self.radius * math.sin(math.radians(360 / num_triangles ))
+        
+        self.rotated_points = [
+            (
+                point[0] * math.cos(math.radians(angle_center + 90)) - point[1] * math.sin(math.radians(angle_center + 90)) + x,
+                point[0] * math.sin(math.radians(angle_center + 90)) + point[1] * math.cos(math.radians(angle_center + 90)) + y
+            )
+            for point in [
+                (-triangle_width / 2, self.length / 2),
+                (triangle_width / 2, self.length / 2),
+                (0, -self.height / 2)
+            ]
+        ]
+
+        # Draw the triangle with the bottom corner always pointing towards the center
+        pygame.draw.polygon(screen, self.color, self.rotated_points)
+
+    def collidepoint(self, point):
+        """
+        Check if the given point is inside the bounding rectangle of the triangle.
+        """
+        x, y = point
+        x_min = min(p[0] for p in self.rotated_points)
+        x_max = max(p[0] for p in self.rotated_points)
+        y_min = min(p[1] for p in self.rotated_points)
+        y_max = max(p[1] for p in self.rotated_points)
+        return x_min <= x <= x_max and y_min <= y <= y_max
+
+        
+
